ff91e82b59d3f16ad7f76a885269fb3cccf18de0
Dean-Overton <64393357+Dean-Overton@users.noreply.github.com>
2021-10-24 20:41:44 +1100

    Blackhole Added

---
+++ Assets/Scripts/QuestControls/Blackhole.cs
@@ -0,0 +1,41 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackhole : MonoBehaviour
+{
+    public float pullRange = 5f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Collider2D[] effectedColliders = Physics2D.OverlapCircleAll(transform.position, pullRange);
+        if (effectedColliders.Length == 0)
+            return;
+        foreach (Collider2D col in effectedColliders) {
+            Rigidbody2D rB = null; 
+            if (col.gameObject.TryGetComponent<Rigidbody2D>(out rB)) {
+                if (rB.gameObject != this.gameObject)
+                    PullObject(col.gameObject, rB);
+            }
+        }
+    }
+    public void PullObject (GameObject effectedGameObject, Rigidbody2D rB) {
+        Vector2 direction = transform.position - effectedGameObject.transform.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = (GetComponent<Rigidbody2D>().mass * rB.mass) / distance;
+
+        Vector2 pullForce = direction.normalized * forceMagnitude;
+        rB.AddForce(pullForce, ForceMode2D.Force);
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        Rigidbody2D rB = other.gameObject.GetComponent<Rigidbody2D>();
+        if (rB) //kill only rigidbodies
+            Destroy(other.gameObject);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, pullRange);
+    }
+}
