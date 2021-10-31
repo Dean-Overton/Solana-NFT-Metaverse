b0113bdd8c1a711e47a356a6e3b2133855270926
Dean-Overton <64393357+Dean-Overton@users.noreply.github.com>
2021-10-31 18:41:37 +1100

    Added hint manager and blackhole cooldown

--- Assets/Scripts/QuestControls/BlackHolePower.cs
+++ Assets/Scripts/QuestControls/BlackHolePower.cs
@@ -4,22 +4,27 @@ using System.Collections;
 public class BlackHolePower : MonoBehaviour
 {
     [SerializeField] private GameObject blackholePrefab;
+    [SerializeField] private float timeBetweenCast = 3f;
+    public bool canCast = true;
 
-    private void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            transform.parent.parent.GetComponent<Animator>().SetTrigger("CastSpell");
-            StartCoroutine("SpawnBlackHole");
-        }
+    float time;
+    private void Start() {
+        time = 0f;
     }
-    public IEnumerator SpawnBlackHole()
-    {
-        Vector3 worldPoint = Input.mousePosition;
-        worldPoint.z = Mathf.Abs(Camera.main.transform.position.z);
-        //worldPoint.z = 11f;
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(worldPoint);
-        mouseWorldPosition.z = 0f;
-        yield return new WaitForSeconds(0.5f);
-        GameObject hole = Instantiate(blackholePrefab, mouseWorldPosition, Quaternion.identity);
-        hole.GetComponent<Blackhole>().playerSpawned = true;
+    private void Update() {
+        time -= Time.deltaTime;
+        if (time <= 0 && canCast) {
+            if (Input.GetMouseButtonDown(0)) {
+                transform.parent.parent.GetComponent<Animator>().SetTrigger("CastSpell");
+                Vector3 worldPoint = Input.mousePosition;
+                worldPoint.z = Mathf.Abs(Camera.main.transform.position.z);
+                //worldPoint.z = 11f;
+                Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(worldPoint);
+                mouseWorldPosition.z = 0f;
+                GameObject hole = Instantiate(blackholePrefab, mouseWorldPosition, Quaternion.identity);
+                hole.GetComponent<Blackhole>().playerSpawned = true;
+                time = timeBetweenCast;
+            }
+        }
     }
 }
