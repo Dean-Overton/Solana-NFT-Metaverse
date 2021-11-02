05fe4e4350d0e26faa13b5890545a6bebda9cb43
Sean Overton <sean14o8.so@gmail.com>
2021-11-02 11:16:58 +1100

    Added moving flames and updated collisions to work correctly

---
+++ Assets/Scripts/QuestControls/Teleport.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 worldPoint = Input.mousePosition;
+            worldPoint.z = Mathf.Abs(Camera.main.transform.position.z);
+            //  Debug.Log(worldPoint);
+            
+            //worldPoint.z = 11f;
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(worldPoint);
+            mouseWorldPosition.z = 0f;
+            Debug.Log(mouseWorldPosition);
+
+            transform.position = mouseWorldPosition;
+        }
+    }
+}
